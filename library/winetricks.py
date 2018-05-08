7135f1a53f5cb3278c3c7ebf412227ffded91549
Luke Short <ekultails@gmail.com>
2018-05-08 12:25:39 -0400

    Renamed the argument 'winetricks_binary' to 'binary'.

--- library/winetricks.py
+++ library/winetricks.py
@@ -22,7 +22,7 @@ options:
         description:
             - The arguments to provide to the Winetricks command
         required: true
-    winetricks_binary:
+    binary:
         description:
             - Specify a custom path to a different Winetricks binary.
         required: false
@@ -34,7 +34,7 @@ author:
 EXAMPLES = '''
 - name: Install the Visual Basic C++ Redistributable 2013
   winetricks:
-    winetricks_binary: /usr/bin/winetricks-20170823
+    binary: /usr/bin/winetricks-20170823
     args: vcrun2013
 '''
 
@@ -52,31 +52,32 @@ stderr:
 
 from ansible.module_utils.basic import AnsibleModule
 
-winetricks_binary = None
+binary = None
 
 def run_module():
 
     module = AnsibleModule(
         argument_spec=dict(
             args=dict(type='str', required=True),
-            winetricks_binary=dict(type='str', required=False),
+            binary=dict(type='str', required=False),
         ),
         supports_check_mode=False
     )
 
-    global winetricks_binary
+    global binary
 
-    # Find and set the Winetricks binary path if it was not provided by the "winetricks_binary" argument.
-    if not winetricks_binary:
-        winetricks_binary = module.get_bin_path('winetricks', required=True)
-        module.params['winetricks_binary'] = winetricks_binary
+    # Find and set the Winetricks binary path if it was not provided by the "binary" argument.
+    if not binary:
+        binary = module.get_bin_path('winetricks', required=True)
+        module.params['binary'] = binary
 
     result = dict(
         changed=True
     )
 
     # Put together the full command that will be run on the managed sytem.
-    cmd = [winetricks_binary, "--unattended", module.params['args']]
+    # The "--unattended" is required to run it in headless mode.
+    cmd = [binary, "--unattended", module.params['args']]
     result['cmd'] = cmd
     # Run the command.
     rc, stdout, stderr = module.run_command(cmd)
